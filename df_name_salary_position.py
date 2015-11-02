65a94cdc87d789f44155fd92c26bc1ca0b0ad1fb
thechutrain <chu.chuyalan@gmail.com>
2015-11-02 10:56:40 -0500

    NOT WORKING! Max salary comes out as 203,000 that's not the max salary

--- df_name_salary_position.py
+++ df_name_salary_position.py
@@ -14,54 +14,54 @@ def df_name_salary_position(year_str, sort_boolean):
     position_list = data[year_str]["position_list"]
     employee_list = data[year_str]["employee_list"] 
     ## DEBUGGING:
-    print len(salary_float)
-    print len(position_list)
-    print len(employee_list)
-    check_list = [1,100,500,1000,2000]
-    for i in range(len(check_list)):
-    	index = check_list[i]
-    	print ("******************")
-    	print employee_list[index]
-    	print salary_float[index]
-    	print position_list[index]
+    # print len(salary_float)
+    # print len(position_list)
+    # print len(employee_list)
+    # check_list = [1,100,500,1000,2000]
+    # for i in range(len(check_list)):
+    # 	index = check_list[i]
+    # 	print ("******************")
+    # 	print employee_list[index]
+    # 	print salary_float[index]
+    # 	print position_list[index]
 
     #these will be the copies of the original lists, but without upl employees
-#     e_list = []
-#     p_list = []
-#     s_float = []
+    e_list = []
+    p_list = []
+    s_float = []
     
-#     ## Take care of the UPL salary option ###
-#     for i in range(len(salary_float)):
-#         if (salary_float[i] != ""):
-#             pass
-# #             salary_float[i] = '{:20,.2f}'.format(salary_float[i])
-#         else:
-#             index_hits.append(i)
-#             salary_float[i] = 0
+    ## Take care of the UPL salary option ###
+    for i in range(len(salary_float)):
+        if (salary_float[i] != ""):
+            pass
+#             salary_float[i] = '{:20,.2f}'.format(salary_float[i])
+        else:
+            index_hits.append(i)
+            salary_float[i] = 0
             
-#     for j in range(len(employee_list)):
-#         if (j in index_hits):
-#             pass
-#         else:
-#             e_list.append(employee_list[j])
-#             p_list.append(position_list[j])
-#             s_float.append(salary_float[j])
-# #     # Create a dataframe!!      
-#     # data = zip(e_list, p_list, s_float)
-#     data = zip(employee_list, position_list, salary_float)
-#     df = pd.DataFrame(data, columns=["Name", "Position", "Salary"])
-#     if (sort_boolean):
-#     	df = df.sort(["Salary"], ascending=False)
-#     return df
+    for j in range(len(employee_list)):
+        if (j in index_hits):
+            pass
+        else:
+            e_list.append(employee_list[j])
+            p_list.append(position_list[j])
+            s_float.append(salary_float[j])
+#     # Create a dataframe!!      
+    # data = zip(e_list, p_list, s_float)
+    data = zip(employee_list, position_list, salary_float)
+    df = pd.DataFrame(data, columns=["Name", "Position", "Salary"])
+    if (sort_boolean):
+    	df = df.sort(["Salary"], ascending=False)
+    return df
     
         
 
 
  #### CALL FUNCTION / TESTING #####
  ## if dataframe:
-# a = df_name_salary_position("00", True)
-# print (a.describe())
+a = df_name_salary_position("00", True)
+print (a.describe())
 # print a
 
 ### No dataframe
-df_name_salary_position("00", True)
+# df_name_salary_position("00", True)

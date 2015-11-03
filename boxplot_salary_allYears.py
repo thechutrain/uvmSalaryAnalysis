48587c05a14d247a23495bb92463647989563d62
thechutrain <chu.chuyalan@gmail.com>
2015-11-02 20:14:06 -0500

    My FIRST GRAPH of data from all the year - its awesome

---
+++ boxplot_salary_allYears.py
@@ -0,0 +1,51 @@
+'''
+The above code works,
+now its time for me to make it into a cleaner function
+'''
+
+from allYears_listData_dict import allYears_listData_dict
+
+import pandas as pd
+import matplotlib.pyplot as plt
+pd.options.display.mpl_style = 'default'
+
+
+### This will call a function that returns a dataframe for each year
+def boxplot_salary_allYears(master_dict):
+    #code
+    years = ["96", "97", "98", "99", "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14"] 
+    for i in range(len(years)):
+        y = years[i]  # gives you the year number as a string
+        #salary for the year, including the unpaid leave values
+        salary_upl = master_dict[y]["salary_float"]
+        salary = []
+        total_salary = 0
+        employees = 0
+        for k in range(len(salary_upl)):
+            try:
+                total_salary += salary_upl[k]
+                salary.append(salary_upl[k])
+                employees +=1
+            except:
+                # when you get an unpaid leave or upl
+                pass
+        if (i == 0):
+            s = pd.DataFrame(salary, columns=[y])
+        else:
+            r = pd.DataFrame(salary, columns=[y])
+            s = pd.concat([s, r], axis=1)
+    return s # s is a dataframe object 
+
+
+###### Call the function ######            
+master = allYears_listData_dict()
+df = boxplot_salary_allYears(master)
+plt.figure()
+df.boxplot(return_type='axes')
+## add title and label axis
+plt.ylabel('Salary (USD)')
+plt.xlabel("School Year")
+plt.title("Distribution of UVM employee salary from 1996 - 2014")
+plt.show(block=True)
+
+    
